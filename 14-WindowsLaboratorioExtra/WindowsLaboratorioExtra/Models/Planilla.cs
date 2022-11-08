f28b61da9701c582060caf9de197ca76526e4b35
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-11-08 20:02:34 -0300

    Escuela Extra-Finalizado

--- 14-WindowsLaboratorioExtra/WindowsLaboratorioExtra/Models/Planilla.cs
+++ 14-WindowsLaboratorioExtra/WindowsLaboratorioExtra/Models/Planilla.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WindowsLaboratorioExtra.Models
 {
+    [Table("Planilla")]
     public class Planilla
     {
         public int PlanillaId { get; set; }
@@ -16,6 +18,25 @@ namespace WindowsLaboratorioExtra.Models
         public int IdProfesor { get; set; }
         public int IdCurso { get; set; }
 
+        public int IdDetalles { get; set; } // o es una lista de id de detalles??
+
+
+
+        #region
+        [ForeignKey("IdCarrera")]
+        public Carrera Carrera { get; set; }
+
+        [ForeignKey("IdMateria")]
+        public Materia Materia { get; set; }
+
+        [ForeignKey("IdCurso")]
+        public Curso Curso { get; set; }
+
+        [ForeignKey("IdProfesor")]
+        public Profesor Profesor { get; set; }
+        [ForeignKey("IdDetalle")]
+        public List<Detalle> detalles { get; set; }
+        #endregion
 
     }
 }
--- 14-WindowsLaboratorioExtra/WindowsLaboratorioExtra/Models/Profesor.cs
+++ 14-WindowsLaboratorioExtra/WindowsLaboratorioExtra/Models/Profesor.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WindowsLaboratorioExtra.Models
 {
+    [Table("Profesor")]
     public class Profesor
     {
         public int ProfesorId { get; set; }
@@ -13,8 +15,16 @@ namespace WindowsLaboratorioExtra.Models
         public string Apellido { get; set; }
 
 
-
+        public int IdPlanilla { get; set; }
         public int IdLocalidad { get; set; }
+
+        #region
+        //[ForeignKey("IdPlanilla")]
+        public List<Planilla> planillas { get; set; }
+
+        [ForeignKey("IdLocalidad")]
+        public Localidad localidad { get; set; }
+        #endregion
 
     }
 }
